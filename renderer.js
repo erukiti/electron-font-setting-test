264899bd6071163a458a8b133b8d8a034bcbae08
erukiti <erukiti@gmail.com>
2015-12-13 20:29:22 +0900

    いい感じにほか修正

--- renderer.js
+++ renderer.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var Rx = require('rx')
-var wx = require('./node_modules/webrx/dist/web.rx')
+const Rx = require('rx')
+const wx = require('./node_modules/webrx/dist/web.rx')
 
 // var ipc = require('ipc')
-var remote = require('electron').remote
+const remote = require('electron').remote
 
-var fontManager = require('font-manager')
+const fontManager = require('font-manager')
 
 wx.app.component('font-selector', {
 	viewModel: function(params) {
@@ -25,15 +25,44 @@ wx.app.component('font-selector', {
 			for (var style in m[family]) {
 				styles.push(m[family][style])
 			}
-			this.fonts.push({family: family})
+			// name = m[family][]
+			let name = family
+			this.fonts.push({family: name, styles: styles})
 		}
+
+		this.fontFamily = wx.property(this.fonts.get(0).family)
+		this.fontFamily.changed.subscribe((family) => {
+			console.dir(m[family])
+		})
+
+		this.sizes = wx.list(['10px', '11px', '12px', '13px', '14px', '15px', '16px', '17px', '18px', '19px', '20px'])
+		this.size = wx.property('16px')
+		this.weights = wx.list([100, 200, 300, 400, 500, 600, 700, 800, 900])
+		this.weight = wx.property(400)
+		this.styles = wx.list(['normal', 'italic', 'oblique'])
+		this.style = wx.property('normal')
 	},
 	template: `
-	<div data-bind="foreach: fonts">
-		<div data-bind="style: {fontFamily: family}">
-			<span data-bind="text: family"></span>
-		</div>
+	<div>
+		<select data-bind="foreach: fonts, selectedValue: @fontFamily">
+			<option data-bind="value: family, text: family"></option>
+		</select>
+		<select data-bind="foreach: sizes, selectedValue: @size">
+			<option data-bind="value: $data, text: $data"></option>
+		</select>
+		<select data-bind="foreach: styles, selectedValue: @style">
+			<option data-bind="value: $data, text: $data"></option>
+		</select>
+		<select data-bind="foreach: weights, selectedValue: @weight">
+			<option data-bind="value: $data, text: $data"></option>
+		</select>
 	</div>
+	<div data-bind="style: {fontFamily: fontFamily, fontStyle: style, fontWeight: weight, fontSize: size}">
+		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+		<div>0123456789</div>
+		<div>「ではみなさんは、そういうふうに川だと云われたり、乳の流れたあとだと云われたりしていたこのぼんやりと白いものがほんとうは何かご承知ですか。」先生は、黒板に吊した大きな黒い星座の図の、上から下へ白くけぶった銀河帯のようなところを指しながら、みんなに問をかけました。</div>
+	</div>
+</div>
 	`
 })
 
